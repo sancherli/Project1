df004add4f1dbe4bc11a5314683fab6206f03b27
Sancher Li <SancherLi96@gmail.com>
2026-08-30 22:18:18 +0500

    add categories and apps

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import App, Category
 
+admin.site.register(App)
+admin.site.register(Category)
 # Register your models here.
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class App(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
